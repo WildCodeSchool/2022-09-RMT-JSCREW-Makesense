66f3d66584260a7c70f5699ef6aae015a54c4421
Chrisdu44 <christopherguichard44@gmail.com>
2022-12-14 11:11:07 +0100

    ajout route back decision

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,13 +1,9 @@
 const express = require("express");
 
+const decisionController = require("./controllers/decisionController");
+
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
-
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+router.get("/decisions", decisionController.browse);
 
 module.exports = router;
