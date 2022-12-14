a0e6e0822bf2df0202dfe95502fa1b6c6bc3cb7b
Chrisdu44 <christopherguichard44@gmail.com>
2022-12-14 11:49:12 +0100

    ajout route back pour tous les utilisateurs

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,6 +5,7 @@ const decisionController = require("./controllers/decisionController");
 const designatedUserController = require("./controllers/designatedUserController");
 const adviceController = require("./controllers/adviceController");
 const conflictController = require("./controllers/conflictController");
+const userController = require("./controllers/userController");
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.get("/decisions", decisionController.browse);
 router.get("/designatedUser", designatedUserController.browse);
 router.get("/advices", adviceController.browse);
 router.get("/conflicts", conflictController.browse);
+router.get("/users", userController.browse);
 
 module.exports = router;
