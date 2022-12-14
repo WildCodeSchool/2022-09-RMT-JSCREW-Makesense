17f8c3253a02587b5960381d522f16ecc1a0ffe9
Chrisdu44 <christopherguichard44@gmail.com>
2022-12-14 11:35:51 +0100

    ajout route back personnes impactées et expertes

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const decisionMakingController = require("./controllers/decisionMakingController");
 const decisionController = require("./controllers/decisionController");
+const designatedUserController = require("./controllers/designatedUserController");
 
 const router = express.Router();
 
 router.get("/decisions", decisionMakingController.browse);
 router.get("/decisions", decisionController.browse);
+router.get("/designatedUser", designatedUserController.browse);
 
 module.exports = router;
