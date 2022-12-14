9397814d54abfbb7a2f2cc1fe643cd65635bad51
Chrisdu44 <christopherguichard44@gmail.com>
2022-12-14 11:45:53 +0100

    ajout route back pour les conflits

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,7 @@ const decisionMakingController = require("./controllers/decisionMakingController
 const decisionController = require("./controllers/decisionController");
 const designatedUserController = require("./controllers/designatedUserController");
 const adviceController = require("./controllers/adviceController");
+const conflictController = require("./controllers/conflictController");
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get("/decisions", decisionMakingController.browse);
 router.get("/decisions", decisionController.browse);
 router.get("/designatedUser", designatedUserController.browse);
 router.get("/advices", adviceController.browse);
+router.get("/conflicts", conflictController.browse);
 
 module.exports = router;
