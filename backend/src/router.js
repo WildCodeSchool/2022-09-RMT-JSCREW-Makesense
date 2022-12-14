fff342c4a91a13bed995db4a7f1cec9bd79ddcb9
Chrisdu44 <christopherguichard44@gmail.com>
2022-12-14 11:55:04 +0100

    nommage des routes get

--- backend/src/router.js
+++ backend/src/router.js
@@ -9,9 +9,9 @@ const userController = require("./controllers/userController");
 
 const router = express.Router();
 
-router.get("/decisions", decisionMakingController.browse);
+router.get("/decisionsMaking", decisionMakingController.browse);
 router.get("/decisions", decisionController.browse);
-router.get("/designatedUser", designatedUserController.browse);
+router.get("/roles", designatedUserController.browse);
 router.get("/advices", adviceController.browse);
 router.get("/conflicts", conflictController.browse);
 router.get("/users", userController.browse);
