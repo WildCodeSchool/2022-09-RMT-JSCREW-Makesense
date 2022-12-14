275e727d0019b9af8e55f2b45ef50a70262904ba
Chrisdu44 <christopherguichard44@gmail.com>
2022-12-14 11:40:58 +0100

    ajout route back pour les avis

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,13 @@ const express = require("express");
 const decisionMakingController = require("./controllers/decisionMakingController");
 const decisionController = require("./controllers/decisionController");
 const designatedUserController = require("./controllers/designatedUserController");
+const adviceController = require("./controllers/adviceController");
 
 const router = express.Router();
 
 router.get("/decisions", decisionMakingController.browse);
 router.get("/decisions", decisionController.browse);
 router.get("/designatedUser", designatedUserController.browse);
+router.get("/advices", adviceController.browse);
 
 module.exports = router;
