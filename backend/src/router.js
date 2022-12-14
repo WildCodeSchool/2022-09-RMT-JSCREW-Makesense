95b07e8915d5fbb91c669a5626677fd6af79e08d
Chrisdu44 <christopherguichard44@gmail.com>
2022-12-14 15:06:52 +0100

    suppression du fichier db.js + renommage des controllers

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,19 +1,19 @@
 const express = require("express");
 
-const decisionMakingController = require("./controllers/decisionMakingController");
-const decisionController = require("./controllers/decisionController");
-const designatedUserController = require("./controllers/designatedUserController");
-const adviceController = require("./controllers/adviceController");
-const conflictController = require("./controllers/conflictController");
-const userController = require("./controllers/userController");
-
 const router = express.Router();
 
-router.get("/decisionsMaking", decisionMakingController.browse);
-router.get("/decisions", decisionController.browse);
-router.get("/roles", designatedUserController.browse);
-router.get("/advices", adviceController.browse);
-router.get("/conflicts", conflictController.browse);
-router.get("/users", userController.browse);
+const DecisionMakingController = require("./controllers/DecisionMakingController");
+const DecisionController = require("./controllers/DecisionController");
+const DesignatedUserController = require("./controllers/DesignatedUserController");
+const AdviceController = require("./controllers/AdviceController");
+const ConflictController = require("./controllers/ConflictController");
+const UserController = require("./controllers/UserController");
+
+router.get("/decisionsMaking", DecisionMakingController.browse);
+router.get("/decisions", DecisionController.browse);
+router.get("/roles", DesignatedUserController.browse);
+router.get("/advices", AdviceController.browse);
+router.get("/conflicts", ConflictController.browse);
+router.get("/users", UserController.browse);
 
 module.exports = router;
