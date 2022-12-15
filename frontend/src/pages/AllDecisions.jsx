ddc4c315c38f173b2381531dd28cc5f812f64429
JoyMarkarian <jmarkarian@hotmail.fr>
2022-12-15 11:41:34 +0100

    Création des composants DecisionCard et DecisionList et de la page AllDecisions

---
+++ frontend/src/pages/AllDecisions.jsx
@@ -0,0 +1,13 @@
+import Search from "../components/Search";
+import DecisionList from "../components/DecisionList";
+
+function AllDecisions() {
+  return (
+    <div>
+      <Search />
+      <DecisionList />
+    </div>
+  );
+}
+
+export default AllDecisions;
