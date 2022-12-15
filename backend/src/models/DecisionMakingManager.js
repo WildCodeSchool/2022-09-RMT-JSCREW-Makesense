12cb427779033d622acfb5ac1b0f1a2be67b2bbb
JoyMarkarian <jmarkarian@hotmail.fr>
2022-12-15 12:24:50 +0100

    Mise à jour de la requête avec inner join sur decisionStatus

--- backend/src/models/DecisionMakingManager.js
+++ backend/src/models/DecisionMakingManager.js
@@ -6,8 +6,10 @@ class DecisionMakingManager extends AbstractManager {
   }
 
   findAll() {
-    return this.connection.query(`select dm.id, dm.title, dm.description, dm.impact, dm.profit, dm.risk, dm.decisionStatus, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, u.firstname, u.lastname  from  ${this.table} as dm
-    inner join user as u on u.id=dm.user_id`);
+    return this.connection
+      .query(`select dm.id, dm.title, dm.description, dm.impact, dm.profit, dm.risk, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, u.firstname, u.lastname, dm.decisionStatus_id, ds.status from ${this.table} as dm 
+    inner join user as u on u.id = dm.user_id 
+    inner join decisionStatus as ds on ds.id = dm.decisionStatus_id`);
   }
 }
 
