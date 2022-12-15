ae5c38bee5ecfb55bd88981b41e362e71c608971
JoyMarkarian <jmarkarian@hotmail.fr>
2022-12-15 12:04:32 +0100

    Modification route DecisionsMaking avec innerjoin sur table User et précision des champs

--- backend/src/models/DecisionMakingManager.js
+++ backend/src/models/DecisionMakingManager.js
@@ -4,6 +4,11 @@ class DecisionMakingManager extends AbstractManager {
   constructor() {
     super({ table: "decisionMaking" });
   }
+
+  findAll() {
+    return this.connection.query(`select dm.id, dm.title, dm.description, dm.impact, dm.profit, dm.risk, dm.decisionStatus, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, u.firstname, u.lastname  from  ${this.table} as dm
+    inner join user as u on u.id=dm.user_id`);
+  }
 }
 
 module.exports = DecisionMakingManager;
