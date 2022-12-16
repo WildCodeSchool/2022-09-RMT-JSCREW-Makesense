74890db800af568271d06efa038693c5b4ac4aa8
guillaume wernert <guil.wernert@gmail.com>
2022-12-16 10:47:38 +0100

    transformation des input en textarea + maj border color au click + margin bottom button

--- frontend/src/pages/NewDecision.jsx
+++ frontend/src/pages/NewDecision.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import "./newDecision.css";
+
 function NewDecision() {
   const [handleDecisionTitle, setHandleDecisonTitle] = useState("");
   const [handleDecisionDescription, setHandleDecisionDescription] =
@@ -42,23 +44,25 @@ function NewDecision() {
             <div className="flex">
               <div className="w-7/12">
                 <div className="mb-5 w-11/12">
-                  <p className="mb-2">Titre de la décision</p>
+                  <p className="mb-2">Titre de la décision*</p>
                   <input
-                    className="border border-500 w-8/12 rounded-lg"
+                    className="border-2 w-8/12 rounded-lg outline-[#c8c8c8]"
                     type="text"
                     id="decisionTitle"
                     name="decisionTitle"
+                    required
                     value={handleDecisionTitle}
                     onChange={handleDecisionInput}
                   />
                 </div>
                 <div className=" mb-5">
-                  <p className="mb-2">Descriptif de la décision</p>
-                  <input
-                    className="border border-500 h-80 w-full rounded-lg"
+                  <p className="mb-2">Descriptif de la décision*</p>
+                  <textarea
+                    className="border-2 border-500 h-80 w-full rounded-lg outline-[#c8c8c8]"
                     type="text"
                     id="decisionDetail"
                     name="decisionDetail"
+                    required
                     value={handleDecisionDescription}
                     onChange={handleDescriptionInput}
                   />
@@ -68,62 +72,67 @@ function NewDecision() {
                 <div className="mb-20">
                   <p className="mb-2">Saisie date</p>
                   <input
-                    className="border border-500 w-8/12 rounded-lg"
+                    className="border-2 border-500 w-8/12 rounded-lg outline-[#c8c8c8]"
                     type="date"
                     value={handleDecisionDate}
                     onChange={handleDecisionDateInput}
                   />
                 </div>
                 <div className="mb-2 ">
-                  <p className="mb-2">Personnes expertes</p>
+                  <p className="mb-2">Personnes expertes*</p>
                   <input
-                    className="border border-500 rounded-lg w-8/12"
+                    className="border-2 border-500 rounded-lg w-8/12 outline-[#c8c8c8]"
                     type="text"
+                    required
                   />
                 </div>
                 <div className="mb-5">
-                  <p className="mb-2">Personnes impactées</p>
+                  <p className="mb-2">Personnes impactées*</p>
                   <input
-                    className="border border-500 rounded-lg w-8/12"
+                    className="border-2 border-500 rounded-lg w-8/12 outline-[#c8c8c8]"
                     type="text"
+                    required
                   />
                 </div>
               </div>
             </div>
             <div className="mb-5 w-full">
-              <p className="mb-2">Impact sur l'organisation</p>
-              <input
-                className="border border-500 h-80 w-7/12 rounded-lg"
+              <p className="mb-2">Impact sur l'organisation*</p>
+              <textarea
+                className="border-2 border-500 h-80 w-7/12 rounded-lg outline-[#c8c8c8]"
                 type="text"
                 id="impact"
                 name="impact"
+                required
                 value={handleImpact}
                 onChange={handleImpactInput}
               />
             </div>
             <div className=" mb-5">
-              <p className="mb-2">Bénéfices</p>
-              <input
-                className="border border-500 h-80  w-7/12 rounded-lg"
+              <p className="mb-2">Bénéfices*</p>
+              <textarea
+                className="border-2 border-500 h-80  w-7/12 rounded-lg outline-[#c8c8c8]"
                 type="text"
                 id="benefits"
                 name="benefits"
+                required
                 value={handleBenefits}
                 onChange={handleBenefitsInput}
               />
             </div>
             <div className=" mb-5">
-              <p className="mb-2">Risques potentiels</p>
-              <input
-                className="border border-500 h-80  w-7/12 rounded-lg"
+              <p className="mb-2">Risques potentiels*</p>
+              <textarea
+                className="border-2 border-500 h-80  w-7/12 rounded-lg outline-[#c8c8c8]"
                 type="text"
                 id="decisionTitle"
                 name="decisionTitle"
+                required
                 value={handleRisk}
                 onChange={handleRiskInput}
               />
             </div>
-            <div className="flex justify-end w-7/12">
+            <div className="flex justify-end w-7/12 mb-5">
               <button
                 type="submit"
                 className="bg-green-900 hover:bg-green-700 w-3/12 h-10 rounded-lg text-white"
