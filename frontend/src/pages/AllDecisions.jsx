443e2a0ea8781fddd87b4a34fe8be78976280e76
JoyMarkarian <jmarkarian@hotmail.fr>
2022-12-20 12:32:33 +0100

    Intégration d'un bouton pour créer une nouvelle prise de décision

--- frontend/src/pages/AllDecisions.jsx
+++ frontend/src/pages/AllDecisions.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+
 import DecisionList from "../components/DecisionList";
 import Search from "../components/Search";
 
 function AllDecisions({ search, handleSearch }) {
   return (
     <div>
-      <Search search={search} handleSearch={handleSearch} />
+      <div className="flex justify-between">
+        <Link to="/user/decision/new">
+          <button
+            type="button"
+            className="ml-10 mt-8 bg-gray-200 rounded-full px-5 py-2 text-ml font-semibold text-gray-700 mr-2 mb-2"
+          >
+            Créer une nouvelle prise de décision
+          </button>
+        </Link>
+        <Search search={search} handleSearch={handleSearch} />
+      </div>
       <DecisionList statusId={1} title="Décision en cours" search={search} />
       <DecisionList
         statusId={2}
