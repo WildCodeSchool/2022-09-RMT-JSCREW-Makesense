ef98c0f92f47be8dd04101c0d546d4d06a9d304d
JoyMarkarian <jmarkarian@hotmail.fr>
2022-12-20 16:09:39 +0100

    Hotfix Archives

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "@components/Navbar";
-import NewDecision from "@pages/NewDecision";
-import ArchivedDecisions from "@pages/ArchivedDecisions";
-import ShowOneDecision from "@pages/ShowOneDecision";
+import NewDecision from "./pages/NewDecision";
+import ArchivedDecisions from "./pages/ArchivedDecisions";
+import ShowOneDecision from "./pages/ShowOneDecision";
 import AllDecisions from "./pages/AllDecisions";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import "./App.css";
@@ -25,7 +25,12 @@ function App() {
           element={<AllDecisions search={search} handleSearch={handleSearch} />}
         />
         <Route path="/user/decision/new" element={<NewDecision />} />
-        <Route path="/archives" element={<ArchivedDecisions />} />
+        <Route
+          path="/archives"
+          element={
+            <ArchivedDecisions search={search} handleSearch={handleSearch} />
+          }
+        />
         <Route path="/onedecision/:id" element={<ShowOneDecision />} />
       </Routes>
       <Footer />
