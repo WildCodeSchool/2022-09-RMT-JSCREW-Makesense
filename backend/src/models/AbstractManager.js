f52b6103a76d7d05ffa25af6d794bb7a99070176
BarzaGuignol <yayav.yaya@gmail.com>
2022-12-20 12:41:30 +0100

    correctif route back findOne

--- backend/src/models/AbstractManager.js
+++ backend/src/models/AbstractManager.js
@@ -9,18 +9,6 @@ class AbstractManager {
     ]);
   }
 
-  findOne(id) {
-    let query = `select dm.id, dm.title, dm.description, dm.impact, dm.profit, dm.risk, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, u.firstname, u.lastname, ds.status from ${this.table} as dm 
-  inner join user as u on u.id = dm.user_id 
-  inner join decisionStatus as ds on ds.id = dm.decisionStatus_id`;
-    const value = [];
-    if (id) {
-      query += " where dm.id = ?";
-      value.push(id);
-    }
-    return this.connection.query(query, value);
-  }
-
   findAll() {
     return this.connection.query(`select * from  ${this.table}`);
   }
--- backend/src/models/DecisionMakingManager.js
+++ backend/src/models/DecisionMakingManager.js
@@ -16,6 +16,18 @@ class DecisionMakingManager extends AbstractManager {
     }
     return this.connection.query(query, value);
   }
+
+  findOne(id) {
+    let query = `select dm.id, dm.title, dm.description, dm.impact, dm.profit, dm.risk, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, u.firstname, u.lastname, ds.status from ${this.table} as dm 
+  inner join user as u on u.id = dm.user_id 
+  inner join decisionStatus as ds on ds.id = dm.decisionStatus_id`;
+    const value = [];
+    if (id) {
+      query += " where dm.id = ?";
+      value.push(id);
+    }
+    return this.connection.query(query, value);
+  }
 }
 
 module.exports = DecisionMakingManager;
