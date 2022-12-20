35d6b136ddda5fa5167de5124e58c4bc5599e460
JoyMarkarian <jmarkarian@hotmail.fr>
2022-12-20 10:46:59 +0100

    Hotfix Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,55 +10,55 @@ import PictoUsers from "@assets/users.svg";
 
 function Navbar() {
   return (
-    <div className="flex justify-between pb-2 bg-gray-200">
-      <img className="ml-10 p-2 pt-4 aspect-auto" src={Logo} alt="logo" />
-      <div className="">
+    <div className="flex justify-between pb-1 bg-gray-200">
+      <img className="ml-10 aspect-auto" src={Logo} alt="logo" />
+      <div>
         <div className="inline-block text-center place-content-center items-center mr-4">
           <img
-            className="inline place-content-center p-2 m-2"
+            className="inline place-content-center p-1 m-1"
             src={PictoHome}
-            alt="logo1"
+            alt="home"
           />
           <p>Parcourir les décisions</p>
         </div>
 
         <div className="inline-block text-center place-content-center mr-4">
           <img
-            className="inline place-content-center p-2 m-2"
+            className="inline place-content-center p-1 m-1"
             src={PictoDecision}
-            alt="logo2"
+            alt="decisions"
           />
           <p>Mes décisions</p>
         </div>
         <div className="inline-block text-center place-content-center mr-4">
           <img
-            className="inline place-content-center p-2 m-2"
+            className="inline place-content-center p-1 m-1"
             src={PictoArchive}
-            alt="logo3"
+            alt="archives"
           />
           <p>Archives</p>
         </div>
         <div className="inline-block text-center place-content-center mr-4">
           <img
-            className="inline place-content-center p-2 m-1"
+            className="inline place-content-center m-1"
             src={PictoUsers}
-            alt="logo4"
+            alt="users"
           />
           <p>Gestion des utilisateurs</p>
         </div>
         <div className="inline-block text-center place-content-center mr-4">
           <img
-            className="inline place-content-center p-2 m-1"
+            className="inline place-content-center m-1"
             src={PictoNotification}
-            alt="logo5"
+            alt="notifications"
           />
           <p>Notifications</p>
         </div>
         <div className="inline-block text-center place-content-center mr-10">
           <img
-            className="inline place-content-center p-2 m-2"
+            className="inline place-content-center p-1 m-1"
             src={PictoLogout}
-            alt="logo6"
+            alt="logout"
           />
           <p>Se déconnecter</p>
         </div>
