de2da77265ce4da85fa7001d8b6cd4a04fb2711d
Tormo <sylvaain11@hotmail.fr>
2022-12-25 17:10:06 +0100

    Avancement CSS/HTML page login

--- frontend/src/pages/Authentification.jsx
+++ frontend/src/pages/Authentification.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+import Logo from "@assets/logo1.svg";
+
 function Authentification() {
-  return <div>Authentification</div>;
+  return (
+    <div>
+      <div className="flex justify-center">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="flex justify-center">
+        <form className="m-5">
+          <div className="group m-3">
+            <label htmlFor="login">Identifiant :</label>
+            <br />
+            <input type="text" name="login" />
+          </div>
+          <div className="group m-3">
+            <label htmlFor="password">Mot de passe :</label>
+            <br />
+            <input type="text" name="password" />
+          </div>
+          <div className="group m-3">
+            <button type="button">Connexion</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Authentification;
