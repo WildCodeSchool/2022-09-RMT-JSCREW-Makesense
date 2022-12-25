e1c71b3723363eadc1c6d17636d5bb2f9d9f775a
Tormo <sylvaain11@hotmail.fr>
2022-12-25 17:26:53 +0100

    Avancement CSS page login

--- frontend/src/pages/Authentification.jsx
+++ frontend/src/pages/Authentification.jsx
@@ -8,19 +8,27 @@ function Authentification() {
       <div className="flex justify-center">
         <img src={Logo} alt="Logo" />
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center card rounded-none">
         <form className="m-5">
           <div className="group m-3">
             <label htmlFor="login">Identifiant :</label>
             <br />
-            <input type="text" name="login" />
+            <input
+              className="border-2 border-500 w-12/12 rounded-lg outline-[#c8c8c8]"
+              type="text"
+              name="login"
+            />
           </div>
           <div className="group m-3">
             <label htmlFor="password">Mot de passe :</label>
             <br />
-            <input type="text" name="password" />
+            <input
+              className="border-2 border-500 w-12/12 rounded-lg outline-[#c8c8c8]"
+              type="text"
+              name="password"
+            />
           </div>
-          <div className="group m-3">
+          <div className="group m-3 flex justify-center">
             <button type="button">Connexion</button>
           </div>
         </form>
