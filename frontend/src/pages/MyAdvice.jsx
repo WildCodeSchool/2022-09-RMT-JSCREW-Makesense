e2e5c0b533e9d3e23c6a480ab9dfd93340b4b71a
BarzaGuignol <yayav.yaya@gmail.com>
2023-01-04 09:59:54 +0100

    mise en page du css

--- frontend/src/pages/MyAdvice.jsx
+++ frontend/src/pages/MyAdvice.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function MyAdvice() {
   return (
-    <div className="pt-20">
-      <div className="text-left ml-20 w-4/5 pr-20">
+    <div className="pt-20 w-[1000px] mx-auto">
+      <div className="text-left w-4/5 pr-20">
         <h2 className="text-emerald-900 text-4xl font-bold mt-2">
           Soumettre un nouvel avis
         </h2>
@@ -11,15 +11,25 @@ export default function MyAdvice() {
       <div>
         <form>
           <textarea
-            className="border-2 border-500 h-80 w-[1000px] ml-20 mt-20 rounded-lg outline-[#c8c8c8]"
+            className="border-2 border-500 h-80 w-full mt-20 rounded-lg outline-[#c8c8c8]"
             type="text"
             id="adviceText"
           />
         </form>
       </div>
-      <div>
-        <button type="button">Annuler</button>
-        <button type="button">Valider</button>
+      <div className="flex justify-end">
+        <button
+          className="bg-emerald-900 hover:bg-emerald-700 text-white py-2 px-16 rounded-full mt-10 text-sm mr-10"
+          type="button"
+        >
+          Annuler
+        </button>
+        <button
+          className="bg-emerald-900 hover:bg-emerald-700 text-white py-2 px-16 rounded-full mt-10 text-sm"
+          type="button"
+        >
+          Valider
+        </button>
       </div>
     </div>
   );
