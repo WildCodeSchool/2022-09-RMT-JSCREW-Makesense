d605c39d3dc1399fd5637ac6f3f7f5ea5fd62c6f
BarzaGuignol <yayav.yaya@gmail.com>
2023-01-04 10:41:51 +0100

    route temporaire pour navigation sur bouton annuler valider

--- frontend/src/pages/MyAdvice.jsx
+++ frontend/src/pages/MyAdvice.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function MyAdvice() {
+  const [myAdviceText, setMyAdviceText] = useState({ advice_text: "" });
+
+  const handleAdvice = (position, value) => {
+    const newAdvice = { ...myAdviceText };
+    newAdvice[position] = value;
+    setMyAdviceText(newAdvice);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="pt-20 w-[1000px] mx-auto">
-      <div className="text-left w-4/5 pr-20">
+    <div className="pt-20 w-[1000px] mx-auto mb-80">
+      <div className="text-left">
         <h2 className="text-emerald-900 text-4xl font-bold mt-2">
           Soumettre un nouvel avis
         </h2>
       </div>
-      <div>
-        <form>
+      <div className="mb-10">
+        <form action="" onSubmit={handleSubmit}>
           <textarea
             className="border-2 border-500 h-80 w-full mt-20 rounded-lg outline-[#c8c8c8]"
             type="text"
             id="adviceText"
+            name="advice_text"
+            required
+            value={myAdviceText.advice_text}
+            onChange={(e) => handleAdvice(e.target.name, e.target.value)}
           />
         </form>
       </div>
-      <div className="flex justify-end">
+      <Link to="/" className="flex justify-end">
         <button
           className="bg-emerald-900 hover:bg-emerald-700 text-white py-2 px-16 rounded-full mt-10 text-sm mr-10"
           type="button"
@@ -30,7 +47,7 @@ export default function MyAdvice() {
         >
           Valider
         </button>
-      </div>
+      </Link>
     </div>
   );
 }
