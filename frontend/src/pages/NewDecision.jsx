c97f3ba32e3df3782a7cb27fb6c220a663be7204
guillaume wernert <guil.wernert@gmail.com>
2023-01-05 17:38:38 +0100

    fix function newDecision const getDate

--- frontend/src/pages/NewDecision.jsx
+++ frontend/src/pages/NewDecision.jsx
@@ -23,7 +23,7 @@ function NewDecision() {
 
   const getDate = () => {
     const date = new Date();
-    return `${date.getDay() + 1}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    return `${date.getDay() + 1}/${date.getMonth() + 1}/${date.getFullYear()}`};
 
   return (
     <div className="w-11/12 ml-20">
@@ -154,6 +154,6 @@ function NewDecision() {
       </form>
     </div>
   );
-}
+};
 
 export default NewDecision;
