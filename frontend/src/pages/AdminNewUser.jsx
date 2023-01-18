c1894872a863fa9e2d9a60554e806f78b8aa1133
Chrisdu44 <christopherguichard44@gmail.com>
2023-01-18 16:54:13 +0100

    ajout pour obliger le remplissage des champs avant validation dans newuser

--- frontend/src/pages/AdminNewUser.jsx
+++ frontend/src/pages/AdminNewUser.jsx
@@ -59,66 +59,62 @@ function AdminNewUser() {
         <h1 className="font-bold text-3xl py-8">
           Renseigner un nouvel utilisateur
         </h1>
-        <form>
+        <form onSubmit={handleAddUser}>
           <div>
-            <p className="pb-4 text-xl">Identifiant</p>
-            <input
-              className="dark:bg-[#e7ebec] dark:text-[#0c3944] border-2 w-1/4 rounded-lg border-[#e7ebec] outline-[#ced7da] mb-10 text-lg"
-              type="text"
-              name="user_username"
-              required="required"
-              value={user.user_username}
-              onChange={(e) => handleNewUser(e.target.name, e.target.value)}
-            />
+            <p className="pb-4 text-xs">* champs obligatoires</p>
           </div>
           <div className="flex justify-start">
             <div className="pr-5">
-              <p className="pb-4 text-xl">Prénom</p>
+              <p className="pb-4 text-xl">Prénom*</p>
               <input
                 className="dark:bg-[#e7ebec] dark:text-[#0c3944] border-2 w-9/10 rounded-lg border-[#e7ebec] outline-[#ced7da] mb-10 text-lg"
                 type="text"
                 name="user_firstname"
                 required="required"
+                placeholder="Saisir le prénom"
                 value={user.user_firstname}
                 onChange={(e) => handleNewUser(e.target.name, e.target.value)}
               />
             </div>
             <div>
-              <p className="pb-4 text-xl">Nom</p>
+              <p className="pb-4 text-xl">Nom*</p>
               <input
                 className="dark:bg-[#e7ebec] dark:text-[#0c3944] border-2 w-9/10 rounded-lg border-[#e7ebec] outline-[#ced7da] mb-10 text-lg"
                 type="text"
                 name="user_lastname"
                 required="required"
+                placeholder="Saisir le nom"
                 value={user.user_lastname}
                 onChange={(e) => handleNewUser(e.target.name, e.target.value)}
               />
             </div>
           </div>
           <div>
-            <p className="pb-4 text-xl">Email</p>
+            <p className="pb-4 text-xl">Email*</p>
             <input
               className="dark:bg-[#e7ebec] dark:text-[#0c3944] border-2 w-1/4 rounded-lg border-[#e7ebec] outline-[#ced7da] mb-10 text-lg"
               type="text"
               name="user_email"
               required="required"
+              placeholder="Saisir l'Email"
               value={user.user_email}
               onChange={(e) => handleNewUser(e.target.name, e.target.value)}
             />
           </div>
           <div>
-            <p className="pb-4 text-xl">Mot de passe</p>
+            <p className="pb-4 text-xl">Mot de passe*</p>
             <input
               className="dark:bg-[#e7ebec] dark:text-[#0c3944] border-2 w-1/4 rounded-lg border-[#e7ebec] outline-[#ced7da] mb-10 text-lg"
               type="text"
               name="user_password"
               required="required"
+              placeholder="Saisir le mot de passe"
               value={user.user_password}
               onChange={(e) => handleNewUser(e.target.name, e.target.value)}
             />
           </div>
           <div>
-            <p className="pb-4 text-xl">Rôle</p>
+            <p className="pb-4 text-xl">Rôle*</p>
             <div className="relative w-52 lg:max-w-sm">
               <select
                 className="p-2.5 text-[#3d6169] bg-white border rounded-md border-[#b6c4c7] shadow-sm outline-none dark:bg-[#e7ebec] mb-10"
@@ -140,7 +136,6 @@ function AdminNewUser() {
             <button
               type="submit"
               className="dark:text-[#0c3944] bg-[#ced7da] rounded-xl px-5 py-2 text-ml font-semibold mr-2 mb-2"
-              onClick={handleAddUser}
             >
               Valider
             </button>
