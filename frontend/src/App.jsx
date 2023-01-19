e89811f5fd9bc08ce7ae26b972c280eed38d876b
JoyMarkarian <jmarkarian@hotmail.fr>
2023-01-19 15:08:01 +0100

    Logique déconnexion & layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import User from "./contexts/User";
-import MyAdvice from "@pages/MyAdvice";
 import Login from "./pages/Login";
 import AdminUsersList from "./pages/AdminUsersList";
 import AdminNewUser from "./pages/AdminNewUser";
@@ -9,13 +7,12 @@ import NewDecision from "./pages/NewDecision";
 import ArchivedDecisions from "./pages/ArchivedDecisions";
 import ShowOneDecision from "./pages/ShowOneDecision";
 import AllDecisions from "./pages/AllDecisions";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Private from "./layout/Private";
 
 import "./App.css";
 
 function App() {
-  const { user } = useContext(User.UserContext);
   const [search, setSearch] = useState("");
   const handleSearch = (value) => {
     setSearch(value);
@@ -23,33 +20,26 @@ function App() {
 
   return (
     <Router>
-      <Navbar user={user} handleUser={user} />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        {(user?.role === "user" || user?.role === "administrator") && (
-          <>
-            <Route
-              path="/"
-              element={
-                <AllDecisions search={search} handleSearch={handleSearch} />
-              }
-            />
-            <Route path="/user/decision/new" element={<NewDecision />} />
-            <Route
-              path="/archives"
-              element={
-                <ArchivedDecisions
-                  search={search}
-                  handleSearch={handleSearch}
-                />
-              }
-            />
-            <Route path="/onedecision/:id" element={<ShowOneDecision />} />
-            <Route path="/decision/:id/advice" element={<MyAdvice />} />
-            <Route path="/users" element={<AdminUsersList />} />
-            <Route path="/users/creation" element={<AdminNewUser />} />
-          </>
-        )}
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Private />}>
+          <Route
+            path="home"
+            element={
+              <AllDecisions search={search} handleSearch={handleSearch} />
+            }
+          />
+          <Route path="user/decision/new" element={<NewDecision />} />
+          <Route
+            path="archives"
+            element={
+              <ArchivedDecisions search={search} handleSearch={handleSearch} />
+            }
+          />
+          <Route path="onedecision/:id" element={<ShowOneDecision />} />
+          <Route path="users" element={<AdminUsersList />} />
+          <Route path="users/creation" element={<AdminNewUser />} />
+        </Route>
       </Routes>
       <Footer />
     </Router>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,13 +16,13 @@ import DarkModeButton from "./DarkModeButton";
 function Navbar() {
   const { user, handleUser } = useContext(User.UserContext);
   const navigate = useNavigate();
-  const handleLogOut = () => {
+  const handleLogOut = (e) => {
     handleUser({});
     navigate("/");
   };
   return (
     <div className="flex justify-between pb-1 bg-[#ced7da] dark:bg-[#6d888f] dark:text-[#e7ebec] font-bold">
-      <Link to="/home">
+      <Link to="/">
         <img className="ml-10 mt-3 aspect-auto" src={Logo} alt="logo" />
       </Link>
       <div className="mt-3">
@@ -30,7 +30,7 @@ function Navbar() {
       </div>
       <div>
         <div className="inline-block text-center place-content-center items-center mr-4">
-          <Link to="/home">
+          <Link to="/">
             <img
               className="inline place-content-center p-1 m-1"
               src={PictoHome}
@@ -38,69 +38,52 @@ function Navbar() {
             />
             <p>Parcourir les décisions</p>
           </Link>
-          <div className="mt-3">
-            <DarkModeButton />
-          </div>
-          <div>
-            <div className="inline-block text-center place-content-center items-center mr-4">
-              <Link to="/">
-                <img
-                  className="inline place-content-center p-1 m-1"
-                  src={PictoHome}
-                  alt="logo1"
-                />
-                <p>Parcourir les décisions</p>
-              </Link>
-            </div>
-            <div className="inline-block text-center place-content-center mr-4">
-              <img
-                className="inline place-content-center p-1 m-1"
-                src={PictoDecision}
-                alt="logo2"
-              />
-              <p>Mes décisions</p>
-            </div>
-            <div className="inline-block text-center place-content-center mr-4">
-              <Link to="/archives">
-                <img
-                  className="inline place-content-center p-1 m-1"
-                  src={PictoArchive}
-                  alt="logo3"
-                />
-                <p>Archives</p>
-              </Link>
-            </div>
-            <div className="inline-block text-center place-content-center mr-4">
-              <Link to="/users">
-                <img
-                  className="inline place-content-center m-1"
-                  src={PictoUsers}
-                  alt="users"
-                />
-                <p>Gestion des utilisateurs</p>
-              </Link>
-            </div>
-            <div className="inline-block text-center place-content-center mr-4">
-              <img
-                className="inline place-content-center m-1"
-                src={PictoNotification}
-                alt="logo5"
-              />
-              <p>Notifications</p>
-            </div>
-            <button
-              type="button"
-              onClick={handleLogOut}
-              className="text-center place-content-center mr-10"
-            >
-              <img className="ml-9 p-1 m-1" src={PictoLogout} alt="logo6" />
-              <p>Se déconnecter</p>
-            </button>
-          </div>
         </div>
-      ) : (
-        ""
-      )}
+        <div className="inline-block text-center place-content-center mr-4">
+          <img
+            className="inline place-content-center p-1 m-1"
+            src={PictoDecision}
+            alt="logo2"
+          />
+          <p>Mes décisions</p>
+        </div>
+        <div className="inline-block text-center place-content-center mr-4">
+          <Link to="/archives">
+            <img
+              className="inline place-content-center p-1 m-1"
+              src={PictoArchive}
+              alt="logo3"
+            />
+            <p>Archives</p>
+          </Link>
+        </div>
+        <div className="inline-block text-center place-content-center mr-4">
+          <Link to="/users">
+            <img
+              className="inline place-content-center m-1"
+              src={PictoUsers}
+              alt="users"
+            />
+            <p>Gestion des utilisateurs</p>
+          </Link>
+        </div>
+        <div className="inline-block text-center place-content-center mr-4">
+          <img
+            className="inline place-content-center m-1"
+            src={PictoNotification}
+            alt="logo5"
+          />
+          <p>Notifications</p>
+        </div>
+        <button
+          type="button"
+          onClick={handleLogOut}
+          className="text-center place-content-center mr-10"
+        >
+          <img className="ml-9 p-1 m-1" src={PictoLogout} alt="logo6" />
+          <p>Se déconnecter</p>
+        </button>
+      </div>
     </div>
   );
 }
