6715ea5dc99d0395a68729cc45dac44908eb8624
BarzaGuignol <yayav.yaya@gmail.com>
2023-01-19 09:59:18 +0100

    rajout lien url vers home sur logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ import DarkModeButton from "./DarkModeButton";
 function Navbar() {
   return (
     <div className="flex justify-between pb-1 bg-[#ced7da] dark:bg-[#6d888f] dark:text-[#e7ebec] font-bold">
-      <Link to="/">
+      <Link to="/home">
         <img className="ml-10 mt-3 aspect-auto" src={Logo} alt="logo" />
       </Link>
       <div className="mt-3">
