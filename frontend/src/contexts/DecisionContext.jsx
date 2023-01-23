35cef2ca9da1c62267733b210c19a031c003d98b
guillaume wernert <guil.wernert@gmail.com>
2023-01-23 15:13:38 +0100

    commentaires des fonctions

--- frontend/src/contexts/DecisionContext.jsx
+++ frontend/src/contexts/DecisionContext.jsx
@@ -3,6 +3,11 @@ import { createContext, useState } from "react";
 const DecisionContext = createContext();
 
 function DecisionProvider({ children }) {
+  /**
+   *maj state creation nouvelle decision
+   * @param {string} position
+   * @param {string} value
+   */
   const [mainDecision, setMainDecision] = useState({
     title: "",
     description: "",
@@ -11,19 +16,39 @@ function DecisionProvider({ children }) {
     risk: "",
   });
 
+  /**
+   *maj du state en fonction de son statut
+   * @param {string} position
+   * @param {string} value
+   */
   const [experts, setExperts] = useState([]);
   const [impacted, setImpacted] = useState([]);
 
+  /**
+   *maj du state personnes experts choisi
+   * @param {string} position
+   * @param {string} value
+   */
   const handleExpert = (addExpert) => {
     const newExpert = [...experts, addExpert];
     setExperts(newExpert);
   };
 
+  /**
+   *maj du state personnes impactées choisi
+   * @param {string} position
+   * @param {string} value
+   */
   const handleImpacted = (addImpacted) => {
     const newImpacted = [...impacted, addImpacted];
     setImpacted(newImpacted);
   };
 
+  /**
+   *maj du state en fonction de la saisie
+   * @param {string} position
+   * @param {string} value
+   */
   const handleMainDecision = (position, value) => {
     const newDecision = { ...mainDecision };
     newDecision[position] = value;
