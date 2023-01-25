d0dc27650ab6cc782b4aac1929a246b97d3f88e0
JoyMarkarian <jmarkarian@hotmail.fr>
2023-01-25 15:11:56 +0100

    Création de la page Mes Décisions

--- backend/src/models/DecisionMakingManager.js
+++ backend/src/models/DecisionMakingManager.js
@@ -6,7 +6,7 @@ class DecisionMakingManager extends AbstractManager {
   }
 
   findAll(id, search) {
-    let query = `select dm.id, dm.title, dm.description, dm.impact, dm.profit, dm.risk, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, u.firstname, u.lastname, dm.decisionStatus_id, ds.status from ${this.table} as dm 
+    let query = `select dm.id, dm.title, dm.description, dm.impact, dm.profit, dm.risk, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, dm.user_id, u.firstname, u.lastname, dm.decisionStatus_id, ds.status from ${this.table} as dm 
   inner join user as u on u.id = dm.user_id
   inner join decisionStatus as ds on ds.id = dm.decisionStatus_id`;
     const value = [];
---
+++ frontend/src/pages/MyDecisions.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+import MyDecisionList from "../components/MyDecisionList";
+import Search from "../components/Search";
+import editMeta from "../services/seo";
+
+function MyDecisions({ search, handleSearch }) {
+  editMeta("Mes décisions");
+
+  return (
+    <div className="min-h-screen dark:bg-[#0c3944]">
+      <div className="flex flex-col sm:flex-row justify-between">
+        <Link
+          to="/user/decision/new"
+          className="text-center ml-6 mr-28 sm:ml-10 mt-4 sm:mt-8 bg-[#ced7da] rounded-xl px-5 py-2 text-ml font-semibold mr-2 sm:mb-2"
+        >
+          Créer une prise de décision
+        </Link>
+        <Search search={search} handleSearch={handleSearch} />
+      </div>
+      <MyDecisionList statusId={1} title="Décisions en cours" search={search} />
+      <MyDecisionList
+        statusId={2}
+        title="Première décision prise"
+        search={search}
+      />
+    </div>
+  );
+}
+
+export default MyDecisions;
