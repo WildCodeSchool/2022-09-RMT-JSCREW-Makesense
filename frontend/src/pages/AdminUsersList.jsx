cd2e61549c61c2bd5ae50acb892489d695544288
Chrisdu44 <christopherguichard44@gmail.com>
2023-01-30 14:47:09 +0100

    ajout de la logique pour la recherche d'un utilisateur dans la liste

--- frontend/src/pages/AdminUsersList.jsx
+++ frontend/src/pages/AdminUsersList.jsx
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import Toast from "@components/Toast";
-import Search from "@components/Search";
 import { Link } from "react-router-dom";
 import { confirmAlert } from "react-confirm-alert";
 
+import Toast from "@components/Toast";
+import Search from "@components/Search";
+
 import editMeta from "../services/seo";
 import apiConnexion from "../services/apiConnexion";
+
 import "react-toastify/dist/ReactToastify.css";
 import "react-confirm-alert/src/react-confirm-alert.css";
 
-function AdminUsersList({ searchPerson, handleSearch }) {
+function AdminUsersList() {
   editMeta("Gestion des utilisateurs");
 
   const [usersList, setUsersList] = useState();
+  const [searchUser, setSearchUSer] = useState("");
 
   const notify = (msg) => {
     toast(msg);
@@ -92,7 +95,7 @@ function AdminUsersList({ searchPerson, handleSearch }) {
   return (
     <div className="dark:bg-[#0c3944]">
       <Toast />
-      <Search search={searchPerson} handleSearch={handleSearch} />
+      <Search search={searchUser} handleSearch={setSearchUSer} />
       <div className="h-min-screen dark:bg-[#0c3944] dark:text-[#e7ebec] py-8">
         <h1 className="flex justify-center font-bold text-3xl sm:px-12 pb-6">
           Gestion des utilisateurs
@@ -114,38 +117,49 @@ function AdminUsersList({ searchPerson, handleSearch }) {
               <th className="w-32">SUPPRIMER</th>
             </tr>
             {usersList &&
-              usersList.map((user) => (
-                <tr className="border">
-                  <td className="p-1.5">
-                    {user.firstname} {user.lastname}
-                  </td>
-                  <td className="hidden sm:inline-table">{user.email}</td>
-                  <td>
-                    <div className="flex justify-center sm:w-52">
-                      <select
-                        className="sm:w-40 text-gray-500 border rounded-md shadow-sm outline-none"
-                        name="user_role"
-                        value={user.role}
-                        onChange={(e) => submit(user, e.target.value)}
-                      >
-                        <option value="administrator">Administrateur</option>
-                        <option value="user">Utilisateur</option>
-                      </select>
-                    </div>
-                  </td>
-                  <td>
-                    <div className="flex justify-center">
-                      <button
-                        className="font-bold text-xl"
-                        type="button"
-                        onClick={() => submit(user)}
-                      >
-                        X
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
+              usersList
+                .filter(
+                  (userToFilter) =>
+                    userToFilter.firstname
+                      .toLowerCase()
+                      .includes(searchUser.toLowerCase()) ||
+                    userToFilter.lastname
+                      .toLowerCase()
+                      .includes(searchUser.toLowerCase()) ||
+                    searchUser === ""
+                )
+                .map((user) => (
+                  <tr className="border">
+                    <td className="p-1.5">
+                      {user.firstname} {user.lastname}
+                    </td>
+                    <td className="hidden sm:inline-table ">{user.email}</td>
+                    <td>
+                      <div className="flex justify-center sm:w-52">
+                        <select
+                          className="sm:w-40 text-gray-500 border rounded-md shadow-sm outline-none"
+                          name="user_role"
+                          value={user.role}
+                          onChange={(e) => submit(user, e.target.value)}
+                        >
+                          <option value="administrator">Administrateur</option>
+                          <option value="user">Utilisateur</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="flex justify-center">
+                        <button
+                          className="font-bold text-xl"
+                          type="button"
+                          onClick={() => submit(user)}
+                        >
+                          X
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
           </table>
         </div>
       </div>
