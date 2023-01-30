92478030d2af810e8af213751c32dfa819e72287
guillaume wernert <guil.wernert@gmail.com>
2023-01-30 14:00:32 +0100

    fix toastify obligation de choisir des personnes

--- frontend/src/pages/NewDecision.jsx
+++ frontend/src/pages/NewDecision.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import SearchPerson from "@components/SearchPerson";
+import Toast from "@components/Toast";
 import { confirmAlert } from "react-confirm-alert";
 import ExportContextDecision from "../contexts/DecisionContext";
 import editMeta from "../services/seo";
@@ -45,10 +46,6 @@ function NewDecision(choosenExperts) {
     }
   };
 
-  const notify = (msg) => {
-    toast(msg);
-  };
-
   /** Fonction qui alerte par un modal de confirmation de la création d'une nouvelle décision */
   function sendFormDecision(e) {
     e.preventDefault();
@@ -65,142 +62,145 @@ function NewDecision(choosenExperts) {
           },
         ],
       });
-    } else notify("Les champs des personnes choisie sont requis");
+    } else toast("Les champs des personnes choisie sont requis");
   }
 
   return (
-    <div className="dark:bg-[#0c3944] dark:text-[#e7ebec] px-6 sm:px-12">
-      <h1 className="font-bold text-3xl py-8">Créer une prise de décision</h1>
-      <form onSubmit={sendFormDecision}>
-        <div className="decision sm:flex">
-          <div className="writeDecision w-full dark:text-[#e7ebec]">
-            <div className="flex">
-              <div className="w-full">
-                <div className="flex">
-                  <div className="w-full sm:w-10/12">
-                    <div className="w-full">
-                      <p className="pb-4 text-xl font-bold">
-                        Titre de la prise de décision*
-                      </p>
-                      <input
-                        className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 w-full rounded-lg border-[#e7ebec] outline-[#ced7da] mb-10 text-lg"
-                        type="text"
-                        id="decisionTitle"
-                        name="title"
-                        required="required"
-                        value={mainDecision.title}
-                        onChange={(e) =>
-                          handleMainDecision(e.target.name, e.target.value)
-                        }
-                      />
-                    </div>
-                    <div>
-                      <p className="pb-4 text-xl font-bold">
-                        Descriptif de la décision*
-                      </p>
-                      <textarea
-                        className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 h-80 w-full border-[#e7ebec] rounded-lg outline-[#ced7da] resize-none mb-10 text-lg"
-                        type="text"
-                        id="decisionDetail"
-                        name="description"
-                        required="required"
-                        value={mainDecision.description}
-                        onChange={(e) =>
-                          handleMainDecision(e.target.name, e.target.value)
-                        }
-                      />
+    <>
+      <Toast />
+      <div className="dark:bg-[#0c3944] dark:text-[#e7ebec] px-6 sm:px-12">
+        <h1 className="font-bold text-3xl py-8">Créer une prise de décision</h1>
+        <form onSubmit={sendFormDecision}>
+          <div className="decision sm:flex">
+            <div className="writeDecision w-full dark:text-[#e7ebec]">
+              <div className="flex">
+                <div className="w-full">
+                  <div className="flex">
+                    <div className="w-full sm:w-10/12">
+                      <div className="w-full">
+                        <p className="pb-4 text-xl font-bold">
+                          Titre de la prise de décision*
+                        </p>
+                        <input
+                          className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 w-full rounded-lg border-[#e7ebec] outline-[#ced7da] mb-10 text-lg"
+                          type="text"
+                          id="decisionTitle"
+                          name="title"
+                          required="required"
+                          value={mainDecision.title}
+                          onChange={(e) =>
+                            handleMainDecision(e.target.name, e.target.value)
+                          }
+                        />
+                      </div>
+                      <div>
+                        <p className="pb-4 text-xl font-bold">
+                          Descriptif de la décision*
+                        </p>
+                        <textarea
+                          className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 h-80 w-full border-[#e7ebec] rounded-lg outline-[#ced7da] resize-none mb-10 text-lg"
+                          type="text"
+                          id="decisionDetail"
+                          name="description"
+                          required="required"
+                          value={mainDecision.description}
+                          onChange={(e) =>
+                            handleMainDecision(e.target.name, e.target.value)
+                          }
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div>
-                  <p className="pb-4 text-xl font-bold">
-                    Impacts sur l'organisation*
-                  </p>
-                  <textarea
-                    className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 h-80 w-full sm:w-10/12 border-[#e7ebec] rounded-lg outline-[#ced7da] resize-none mb-10 text-lg"
-                    type="text"
-                    id="impact"
-                    name="impact"
-                    required="required"
-                    value={mainDecision.impact}
-                    onChange={(e) =>
-                      handleMainDecision(e.target.name, e.target.value)
-                    }
-                  />
-                </div>
-                <div>
-                  <p className="pb-4 text-xl font-bold">Bénéfices*</p>
-                  <textarea
-                    className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 h-80 w-full sm:w-10/12 border-[#e7ebec] rounded-lg outline-[#ced7da] resize-none mb-10 text-lg"
-                    type="text"
-                    id="profit"
-                    name="profit"
-                    required="required"
-                    value={mainDecision.profit}
-                    onChange={(e) =>
-                      handleMainDecision(e.target.name, e.target.value)
-                    }
-                  />
-                </div>
-                <div>
-                  <p className="pb-4 text-xl font-bold">Risques potentiels*</p>
-                  <textarea
-                    className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 h-80 w-full sm:w-10/12 border-[#e7ebec] rounded-lg outline-[#ced7da] resize-none mb-10 text-lg"
-                    type="text"
-                    id="decisionTitle"
-                    name="risk"
-                    required="required"
-                    value={mainDecision.risk}
-                    onChange={(e) =>
-                      handleMainDecision(e.target.name, e.target.value)
-                    }
-                  />
+                  <div>
+                    <p className="pb-4 text-xl font-bold">
+                      Impacts sur l'organisation*
+                    </p>
+                    <textarea
+                      className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 h-80 w-full sm:w-10/12 border-[#e7ebec] rounded-lg outline-[#ced7da] resize-none mb-10 text-lg"
+                      type="text"
+                      id="impact"
+                      name="impact"
+                      required="required"
+                      value={mainDecision.impact}
+                      onChange={(e) =>
+                        handleMainDecision(e.target.name, e.target.value)
+                      }
+                    />
+                  </div>
+                  <div>
+                    <p className="pb-4 text-xl font-bold">Bénéfices*</p>
+                    <textarea
+                      className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 h-80 w-full sm:w-10/12 border-[#e7ebec] rounded-lg outline-[#ced7da] resize-none mb-10 text-lg"
+                      type="text"
+                      id="profit"
+                      name="profit"
+                      required="required"
+                      value={mainDecision.profit}
+                      onChange={(e) =>
+                        handleMainDecision(e.target.name, e.target.value)
+                      }
+                    />
+                  </div>
+                  <div>
+                    <p className="pb-4 text-xl font-bold">Risques potentiels*</p>
+                    <textarea
+                      className="dark:bg-[#ced7da] dark:text-[#0c3944] border-2 h-80 w-full sm:w-10/12 border-[#e7ebec] rounded-lg outline-[#ced7da] resize-none mb-10 text-lg"
+                      type="text"
+                      id="decisionTitle"
+                      name="risk"
+                      required="required"
+                      value={mainDecision.risk}
+                      onChange={(e) =>
+                        handleMainDecision(e.target.name, e.target.value)
+                      }
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="decisionByUser sm:w-6/12">
-            <div className="flex justify-start mb-5 dark:text-[#e7ebec]">
-              <p className="pr-5 text-xl font-bold">Date de création :</p>
-              <p className="text-xl">{new Date().toLocaleDateString()}</p>
-              <p
-                className="hidden text-xl"
-                onChange={(e) =>
-                  handleMainDecision(e.target.name, e.target.value)
-                }
-              >
-                {(mainDecision.dateCreate = getDate())}
-              </p>
-              <p
-                className="hidden ml-6 text-xl"
-                onChange={(e) =>
-                  handleMainDecision(e.target.name, e.target.value)
-                }
-              >
-                {(mainDecision.dateFinalDecision = finalDate())}
-              </p>
+            <div className="decisionByUser sm:w-6/12">
+              <div className="flex justify-start mb-5 dark:text-[#e7ebec]">
+                <p className="pr-5 text-xl font-bold">Date de création :</p>
+                <p className="text-xl">{new Date().toLocaleDateString()}</p>
+                <p
+                  className="hidden text-xl"
+                  onChange={(e) =>
+                    handleMainDecision(e.target.name, e.target.value)
+                  }
+                >
+                  {(mainDecision.dateCreate = getDate())}
+                </p>
+                <p
+                  className="hidden ml-6 text-xl"
+                  onChange={(e) =>
+                    handleMainDecision(e.target.name, e.target.value)
+                  }
+                >
+                  {(mainDecision.dateFinalDecision = finalDate())}
+                </p>
+              </div>
+              <div className="mb-2">
+                <SearchPerson SearchPerson={SearchPerson} id={choosenExperts} />
+              </div>
             </div>
-            <div className="mb-2">
-              <SearchPerson SearchPerson={SearchPerson} id={choosenExperts} />
-            </div>
           </div>
-        </div>
-        <div className="flex justify-end w-10/12 mb-5">
-          <Link
-            to="/home"
-            className="dark:text-[#0c3944] bg-[#ced7da] rounded-xl px-5 py-2 text-ml font-semibold mr-2 mb-2"
-          >
-            Annuler
-          </Link>
-          <button
-            type="submit"
-            className="dark:text-[#0c3944] bg-[#ced7da] rounded-xl px-5 py-2 text-ml font-semibold mr-2 mb-2"
-          >
-            Valider
-          </button>
-        </div>
-      </form>
-    </div>
+          <div className="flex justify-end w-10/12 mb-5">
+            <Link
+              to="/home"
+              className="dark:text-[#0c3944] bg-[#ced7da] rounded-xl px-5 py-2 text-ml font-semibold mr-2 mb-2"
+            >
+              Annuler
+            </Link>
+            <button
+              type="submit"
+              className="dark:text-[#0c3944] bg-[#ced7da] rounded-xl px-5 py-2 text-ml font-semibold mr-2 mb-2"
+            >
+              Valider
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
