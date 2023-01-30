b4d209a31f2d1ce52a52c322d85c4d859de15a56
guillaume wernert <guil.wernert@gmail.com>
2023-01-30 16:00:22 +0100

    logique back pour les personnes choisies

--- backend/src/models/DecisionMakingManager.js
+++ backend/src/models/DecisionMakingManager.js
@@ -5,7 +5,6 @@ class DecisionMakingManager extends AbstractManager {
     super({ table: "decisionMaking" });
   }
 
-
   findAll(statusId, search) {
     let query = `select dm.id, dm.title, dm.description, dm.impact, dm.profit, dm.risk, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, dm.user_id, u.firstname, u.lastname, dm.decisionStatus_id, ds.status from ${this.table} as dm 
   inner join user as u on u.id = dm.user_id
@@ -35,7 +34,7 @@ class DecisionMakingManager extends AbstractManager {
 
   findOne(id) {
     const query = `select dm.id, dm.user_id, dm.title, dm.description, dm.decisionStatus_id, dm.impact, dm.profit, dm.risk, dm.dateCreate, dm.dateAdvice, dm.dateFirstDecision, dm.dateConflict, dm.dateFinalDecision, u.firstname, u.lastname, ds.status, dm.firstDecision, dm.finalDecision from ${this.table} as dm 
-  inner join user as u on u.id = dm.user_id 
+  inner join user as u on u.id = dm.user_id
   inner join decisionStatus as ds on ds.id = dm.decisionStatus_id
   where dm.id = ?`;
     return this.connection.query(query, [id]);
