cbe1f9834c6efc9977e965ce98af2f4dea85a2c6
JoyMarkarian <jmarkarian@hotmail.fr>
2023-02-08 16:08:00 +0100

    Fix Decision Card

--- frontend/src/components/DecisionCard.jsx
+++ frontend/src/components/DecisionCard.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 
-import { Avatar, AvatarDark } from "@assets/";
+import { Avatar, AvatarDark } from "../assets";
 
 function DecisionCard({ decisionsMaking, definitive }) {
   return (
     <Link
-      className="max-w-sm flex flex-col rounded-lg border-2 border-[#e7ebec] overflow-hidden ml-6 sm:ml-10 my-2 w-5/6 sm:w-1/3 px-2"
+      className="max-w-sm flex flex-col rounded-lg border-2 border-[#e7ebec] overflow-hidden ml-6 sm:ml-10 my-2 w-5/6 sm:w-1/3 px-2 h-52"
       to={`/decision/${decisionsMaking.id}`}
     >
       <div className="px-6 pt-4">
@@ -13,26 +13,24 @@ function DecisionCard({ decisionsMaking, definitive }) {
           {definitive ? "Décision archivée" : decisionsMaking.status}
         </span>
       </div>
-      <div className="flex flex-col px-6 py-4 h-1/2">
-        <div className="font-bold text-xl mb-2 dark:text-[#e7ebec]">
-          {decisionsMaking.title}
-        </div>
-        <div>
-          <div className="flex items-center">
-            <img
-              className="w-10 h-10 rounded-full mr-4 dark:hidden"
-              src={Avatar}
-              alt="Avatar of user"
-            />
-            <img
-              className="w-10 h-10 rounded-full mr-4 hidden dark:inline"
-              src={AvatarDark}
-              alt="Avatar of user"
-            />
-            <p className="text-gray-700 text-base text-sm dark:text-[#e7ebec]">
-              Par {decisionsMaking.firstname} {decisionsMaking.lastname}
-            </p>
-          </div>
+      <div className="font-bold text-xl mb-2 dark:text-[#e7ebec] px-6 pt-4 h-1/3">
+        {decisionsMaking.title}
+      </div>
+      <div className="flex flex-col px-6 my-4 h-2/2">
+        <div className="flex items-center">
+          <img
+            className="w-10 h-10 rounded-full mr-4 dark:hidden"
+            src={Avatar}
+            alt="Avatar of user"
+          />
+          <img
+            className="w-10 h-10 rounded-full mr-4 hidden dark:inline"
+            src={AvatarDark}
+            alt="Avatar of user"
+          />
+          <p className="text-gray-700 text-base text-sm dark:text-[#e7ebec]">
+            Par {decisionsMaking.firstname} {decisionsMaking.lastname}
+          </p>
         </div>
       </div>
     </Link>
